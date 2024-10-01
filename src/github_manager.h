b8aa0029b12d71e56a4d1bff98f971340786e47f
ubuntu <ubuntu@localhost.localdomain>
2024-10-01 22:17:45 +0800

    update code

--- src/github_manager.h
+++ src/github_manager.h
@@ -1,5 +1,5 @@
-﻿#ifndef  __GITHUB_HOSTS_H__
-#define  __GITHUB_HOSTS_H__
+﻿#ifndef __GITHUB_MANAGER_H__
+#define __GITHUB_MANAGER_H__
 
 #include "http_client/http_client.h"
 #include "thread_pool/thread_pool.h"
@@ -7,18 +7,18 @@
 class GithubHosts
 {
 public:
-    static GithubHosts* instance();
+    static GithubHosts *instance();
     bool updateGithubHosts();
 
 private:
     GithubHosts();
     ~GithubHosts();
-    bool getUrlNames(std::vector<std::string>& urlNames);
-    std::string getIpByUrl(const std::string& suffixUrl);
-    bool updateHostFile(int taskCount, const std::vector<std::string>& urlNames, 
-        std::vector<std::future<std::string>>& htmlTextList);
-    std::string parseIpByHtml(const std::string& htmlText);
-    int getAverageTimeByPingIp(const std::string& ip);
+    bool getUrlNames(std::vector<std::string> &urlNames);
+    std::string getIpByUrl(const std::string &suffixUrl);
+    bool updateHostFile(int taskCount, const std::vector<std::string> &urlNames,
+                        std::vector<std::future<std::string>> &htmlTextList);
+    std::string parseIpByHtml(const std::string &htmlText);
+    int getAverageTimeByPingIp(const std::string &ip);
 
 private:
     std::unique_ptr<ThreadPool> m_threadPoolPtr;
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include "log/log.h"
 #include "github_manager.h"
 
-
 int main(int argc, char *argv[])
 {
     bool bUpdateRes = GithubHosts::instance()->updateGithubHosts();
     CONSOLE_INFO("update github hosts %s ...", bUpdateRes ? "success" : "failed");
+#ifdef WIN32
     system("pause");
+#endif
     return 0;
 }
